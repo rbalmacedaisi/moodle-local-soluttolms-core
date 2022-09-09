ef5c9fd7d91fcd22dd815498bfd386cdc17f68ea
Alejandro Garcia <alejandro.garcia@soluttoconsulting.com>
2022-09-09 16:27:51 +0000

    SC-07: Web service para obtener información del curso

--- lib.php
+++ lib.php
@@ -83,3 +83,20 @@ function get_messages_from_user_conversations($userid, $limitfrom, $limitnum)
     
     return ['return' => $return, 'usersArray' => $usersArray];
 }
+
+function core_get_course_image($courseid)
+{
+   $url = '';
+   require_once( $CFG->libdir . '/filelib.php' );
+
+   $context = context_course::instance( $courseid );
+   $fs = get_file_storage();
+   $files = $fs->get_area_files( $context->id, 'course', 'overviewfiles', 0 );
+
+   foreach ( $files as $f ){
+        if ( $f->is_valid_image() ){
+            $url = moodle_url::make_pluginfile_url( $f->get_contextid(), $f->get_component(), $f->get_filearea(), null, $f->get_filepath(), $f->get_filename(), false );
+        }
+   }
+   return $url;
+}
